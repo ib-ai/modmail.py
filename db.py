0cfde21def0d14795f44cd76196e01d6dfb103e6
NathanealV <18462634+NathanealV@users.noreply.github.com>
2023-08-26 14:16:31 +0930

    Switched classes to dataclasses. Refactored to use `async_db_cursor` decorator with type hints. Fixed formatting and linting issues.

--- db.py
+++ db.py
@@ -1,237 +1,243 @@
 from contextlib import asynccontextmanager
-import aiosqlite
-from typing import Optional
+from dataclasses import dataclass
+import functools
+from typing import Awaitable, Callable, Concatenate, Optional, ParamSpec, TypeVar
+from aiosqlite import connect, Row, Cursor
 
-path = '/database/modmail.db'
+
+PATH = "/database/modmail.db"
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
 @asynccontextmanager
 async def db_ops():
-    conn = await aiosqlite.connect(path)
-    conn.row_factory = aiosqlite.Row
+    conn = await connect(PATH)
+    conn.row_factory = Row
     cursor = await conn.cursor()
     yield cursor
     await conn.commit()
     await conn.close()
 
 
+def async_db_cursor(
+    func: Callable[Concatenate[Cursor, P], Awaitable[R]]
+) -> Callable[P, Awaitable[R]]:
+    @functools.wraps(func)
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        async with db_ops() as cursor:
+            return await func(cursor, *args, **kwargs)
+
+    return wrapper
+
+
+@dataclass
 class Ticket:
-
-    def __init__(self, ticket_id: int, user: int, open: int,
-                 message_id: Optional[int]):
-        self.ticket_id = ticket_id
-        self.user = user
-        self.open = open
-        self.message_id = message_id
-
-    def __repr__(self) -> str:
-        return f"Ticket({self.ticket_id}, {self.user}, {self.open}, {self.message_id})"
+    ticket_id: int
+    user: int
+    open: int
+    message_id: Optional[int]
 
 
+@dataclass
 class TicketResponse:
-
-    def __init__(self, user: int, response: str, timestamp: int,
-                 as_server: bool):
-        self.user = user
-        self.response = response
-        self.timestamp = timestamp
-        self.as_server = as_server
-
-    def __repr__(self) -> str:
-        return f"TicketResponse({self.user}, {self.response}, {self.timestamp}, {self.as_server})"
+    user: int
+    response: str
+    timestamp: int
+    as_server: bool
 
 
+@dataclass
 class Timeout:
-
-    def __init__(self, timeout_id: int, timestamp: int):
-        self.timeout_id = timeout_id
-        self.timestamp = timestamp
-
-    def __repr__(self) -> str:
-        return f"Timeout({self.timeout_id}, {self.user}, {self.timestamp})"
+    timeout_id: int
+    timestamp: int
 
 
-async def get_ticket(ticket_id: int) -> Optional[Ticket]:
+@async_db_cursor
+async def get_ticket(cursor: Cursor, ticket_id: int) -> Optional[Ticket]:
     sql = """
         SELECT ticket_id, user, open, message_id
         FROM mm_tickets
         WHERE ticket_id=?
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [ticket_id])
-        ticket = await cursor.fetchone()
-        if ticket is None or len(ticket) == 0:
-            return None
-        else:
-            return Ticket(*ticket)
+    await cursor.execute(sql, [ticket_id])
+    ticket = await cursor.fetchone()
+    if ticket is None or len(ticket) == 0:
+        return None
+    else:
+        return Ticket(*ticket)
 
 
-async def get_ticket_by_user(user: int) -> Optional[Ticket]:
+@async_db_cursor
+async def get_ticket_by_user(cursor: Cursor, user: int) -> Optional[Ticket]:
     sql = """
         SELECT ticket_id, user, open, message_id
         FROM mm_tickets
         WHERE user=?
         AND open=1
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [user])
-        ticket = await cursor.fetchone()
-        if ticket is None or len(ticket) == 0:
-            return None
-        else:
-            return Ticket(*ticket)
+    await cursor.execute(sql, [user])
+    ticket = await cursor.fetchone()
+    if ticket is None or len(ticket) == 0:
+        return None
+    else:
+        return Ticket(*ticket)
 
 
-async def get_ticket_by_message(message_id: int) -> Optional[Ticket]:
+@async_db_cursor
+async def get_ticket_by_message(cursor: Cursor, message_id: int) -> Optional[Ticket]:
     sql = """
         SELECT ticket_id, user, open, message_id
         FROM mm_tickets
         WHERE message_id=?
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [message_id])
-        ticket = await cursor.fetchone()
-        if ticket is None or len(ticket) == 0:
-            return None
-        else:
-            return Ticket(*ticket)
+    await cursor.execute(sql, [message_id])
+    ticket = await cursor.fetchone()
+    if ticket is None or len(ticket) == 0:
+        return None
+    else:
+        return Ticket(*ticket)
 
 
-async def open_ticket(user: int) -> Optional[int]:
+@async_db_cursor
+async def open_ticket(cursor: Cursor, user: int) -> Optional[int]:
     sql = """
         INSERT INTO mm_tickets (user)
         VALUES (?)
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [user])
-        return cursor.lastrowid
+    await cursor.execute(sql, [user])
+    return cursor.lastrowid
 
 
-async def update_ticket_message(ticket_id: int, message_id: int) -> bool:
+@async_db_cursor
+async def update_ticket_message(
+    cursor: Cursor, ticket_id: int, message_id: int
+) -> bool:
     sql = """
         UPDATE mm_tickets
         SET message_id=?
         WHERE ticket_id=?
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [message_id, ticket_id])
-        return cursor.rowcount != 0
+    await cursor.execute(sql, [message_id, ticket_id])
+    return cursor.rowcount != 0
 
 
-async def close_ticket(ticket_id: int) -> bool:
+@async_db_cursor
+async def close_ticket(cursor: Cursor, ticket_id: int) -> bool:
     sql = """
         UPDATE mm_tickets
         SET open=0
         WHERE ticket_id=?
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [ticket_id])
-        return cursor.rowcount != 0
+    await cursor.execute(sql, [ticket_id])
+    return cursor.rowcount != 0
 
 
-async def get_ticket_responses(ticket_id: int) -> list[TicketResponse]:
+@async_db_cursor
+async def get_ticket_responses(cursor: Cursor, ticket_id: int) -> list[TicketResponse]:
     sql = """
         SELECT user, response, timestamp, as_server
         FROM mm_ticket_responses
         WHERE ticket_id=?
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [ticket_id])
-        rows = await cursor.fetchall()
-        return [TicketResponse(*row) for row in rows]
+    await cursor.execute(sql, [ticket_id])
+    rows = await cursor.fetchall()
+    return [TicketResponse(*row) for row in rows]
 
 
-async def add_ticket_response(ticket_id: int, user: int, response: str,
-                              as_server: bool) -> Optional[int]:
+@async_db_cursor
+async def add_ticket_response(
+    cursor: Cursor, ticket_id: int, user: int, response: str, as_server: bool
+) -> Optional[int]:
     sql = """
         INSERT INTO mm_ticket_responses (ticket_id, user, response, as_server)
         VALUES (?, ?, ?, ?)
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [ticket_id, user, response, as_server])
-        return cursor.lastrowid
+    await cursor.execute(sql, [ticket_id, user, response, as_server])
+    return cursor.lastrowid
 
 
-async def get_timeout(user: int) -> Optional[Timeout]:
+@async_db_cursor
+async def get_timeout(cursor: Cursor, user: int) -> Optional[Timeout]:
     sql = """
         SELECT timeout_id, timestamp
         FROM mm_timeouts
         WHERE user=?
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [user])
-        timeout = await cursor.fetchone()
-        if timeout is None or len(timeout) == 0:
-            return None
-        else:
-            return Timeout(*timeout)
+    await cursor.execute(sql, [user])
+    timeout = await cursor.fetchone()
+    if timeout is None or len(timeout) == 0:
+        return None
+    else:
+        return Timeout(*timeout)
 
 
-async def set_timeout(user: int, timestamp: int) -> Optional[int]:
+@async_db_cursor
+async def set_timeout(cursor: Cursor, user: int, timestamp: int) -> Optional[int]:
     sql = """
         INSERT OR REPLACE INTO mm_timeouts (user, timestamp)
         VALUES (?, ?)
     """
-    async with db_ops() as cursor:
-        await cursor.execute(sql, [user, timestamp])
-        return cursor.lastrowid
+    await cursor.execute(sql, [user, timestamp])
+    return cursor.lastrowid
 
 
-async def init():
-    async with db_ops() as cursor:
-        # Create modmail tickets table
-        sql = """
-        CREATE TABLE IF NOT EXISTS mm_tickets (
-            ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user INTEGER NOT NULL,
-            open BOOLEAN DEFAULT 1 NOT NULL,
-            message_id INTEGER
-        );
-        """
-        await cursor.execute(sql)
+@async_db_cursor
+async def init(cursor: Cursor):
+    # Create modmail tickets table
+    sql = """
+    CREATE TABLE IF NOT EXISTS mm_tickets (
+        ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user INTEGER NOT NULL,
+        open BOOLEAN DEFAULT 1 NOT NULL,
+        message_id INTEGER
+    );
+    """
+    await cursor.execute(sql)
 
-        # Create modmail ticket user index
-        sql = "CREATE INDEX IF NOT EXISTS mm_tickets_user ON mm_tickets(user);"
-        await cursor.execute(sql)
+    # Create modmail ticket user index
+    sql = "CREATE INDEX IF NOT EXISTS mm_tickets_user ON mm_tickets(user);"
+    await cursor.execute(sql)
 
-        # Create modmail ticket message index
-        sql = "CREATE INDEX IF NOT EXISTS mm_tickets_message ON mm_tickets(message_id);"
-        await cursor.execute(sql)
+    # Create modmail ticket message index
+    sql = "CREATE INDEX IF NOT EXISTS mm_tickets_message ON mm_tickets(message_id);"
+    await cursor.execute(sql)
 
-        # Create modmail ticket repsonses table
-        sql = """
-        CREATE TABLE IF NOT EXISTS mm_ticket_responses (
-            response_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ticket_id INTEGER,
-            user INTEGER NOT NULL,
-            response TEXT NOT NULL,
-            timestamp TIMESTAMP DEFAULT (strftime('%s', 'now')) NOT NULL,
-            as_server BOOLEAN NOT NULL,
-            FOREIGN KEY (ticket_id) REFERENCES mm_tickets (ticket_id)
-        );
-        """
-        await cursor.execute(sql)
+    # Create modmail ticket repsonses table
+    sql = """
+    CREATE TABLE IF NOT EXISTS mm_ticket_responses (
+        response_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ticket_id INTEGER,
+        user INTEGER NOT NULL,
+        response TEXT NOT NULL,
+        timestamp TIMESTAMP DEFAULT (strftime('%s', 'now')) NOT NULL,
+        as_server BOOLEAN NOT NULL,
+        FOREIGN KEY (ticket_id) REFERENCES mm_tickets (ticket_id)
+    );
+    """
+    await cursor.execute(sql)
 
-        # Create modmail ticket response ticket id index
-        sql = "CREATE INDEX IF NOT EXISTS mm_ticket_responses_ticket_id ON mm_ticket_responses(ticket_id);"
-        await cursor.execute(sql)
+    # Create modmail ticket response ticket id index
+    sql = "CREATE INDEX IF NOT EXISTS mm_ticket_responses_ticket_id ON mm_ticket_responses(ticket_id);"
+    await cursor.execute(sql)
 
-        # Create modmail ticket response user index
-        sql = "CREATE INDEX IF NOT EXISTS mm_ticket_responses_user ON mm_ticket_responses(user);"
-        await cursor.execute(sql)
+    # Create modmail ticket response user index
+    sql = "CREATE INDEX IF NOT EXISTS mm_ticket_responses_user ON mm_ticket_responses(user);"
+    await cursor.execute(sql)
 
-        # Create modmail timeouts table
-        sql = """
-        CREATE TABLE IF NOT EXISTS mm_timeouts (
-            timeout_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user INTEGER NOT NULL UNIQUE,
-            timestamp TIMESTAMP DEFAULT (strftime('%s', 'now')) NOT NULL
-        );
-        """
-        await cursor.execute(sql)
+    # Create modmail timeouts table
+    sql = """
+    CREATE TABLE IF NOT EXISTS mm_timeouts (
+        timeout_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user INTEGER NOT NULL UNIQUE,
+        timestamp TIMESTAMP DEFAULT (strftime('%s', 'now')) NOT NULL
+    );
+    """
+    await cursor.execute(sql)
 
-        # Create modmail timeout user index
-        sql = "CREATE UNIQUE INDEX IF NOT EXISTS mm_timeouts_user ON mm_timeouts(user);"
-        await cursor.execute(sql)
+    # Create modmail timeout user index
+    sql = "CREATE UNIQUE INDEX IF NOT EXISTS mm_timeouts_user ON mm_timeouts(user);"
+    await cursor.execute(sql)
 
-        return True
+    return True
